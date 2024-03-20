ef100de9717b42c05389d06e3fdc7474a300416d
Luigi Lizcano León <62868296+luigilizcano1003@users.noreply.github.com>
2024-03-20 00:12:14 -0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -48,7 +48,7 @@ regd_users.post("/login", (req,res) => {
     req.session.authorization = {
       accessToken,username
   }
-  return res.status(200).send("User successfully logged in");
+  return res.status(200).json({message:"User successfully logged in"});
   } else {
     return res.status(208).json({message: "Invalid Login. Check username and password"});
   }
@@ -58,23 +58,49 @@ regd_users.post("/login", (req,res) => {
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
   const isbn = req.params.isbn;
-  const username = req.session.authorization.username;
   const review = req.body.review;
-  let l = Object.keys(books[isbn]).review.length + 1;
-  Object.keys.books[isbn].reviews.forEach(element => {
-    if( username == books[isbn].reviews[element].username){
-      books[isbn].reviews[element].review = review
+  let l = Object.keys(books[isbn].reviews).length + 1;
+  if(Object.keys(books[isbn].reviews).length > 0){
+    let i = 0;
+    Object.keys(books[isbn].reviews).forEach(element => {      
+      if( req.session.authorization.username == books[isbn].reviews[element].username){
+        books[isbn].reviews[element].review = review;
+        i=i+1;
+      }
+    })
+    if(i==0){
+      Object.assign(books[isbn].reviews,{[l]:{"username":req.session.authorization.username,"review":review}})
     }
-    else{
-      books[isbn].reviews={Object.keys(books[isbn]).review.length + 1:{"username":username,"review":review}}
+  }
+  else{
+    Object.assign(books[isbn].reviews,{[l]:{"username":req.session.authorization.username,"review":review}})
     }
-  });
-  //if (isbn){
-  //  books[isbn].reviews={1:{"username": username,"review":review}}
-  //    }
+  return res.status(200).send(`The username ${req.session.authorization.username} has done a review; ${review} for book with ISBN ${isbn}`);
+});
 
-
-  return res.status(200).send(`The username ${username} has done a review for book with ISBN ${isbn}`);
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const l= Object.keys(books[isbn].reviews).length;
+  let j=0;
+  Object.keys(books[isbn].reviews).forEach(element =>{
+    if( req.session.authorization.username == books[isbn].reviews[element].username){
+      j=element;
+      }
+    });    
+  if(j>0){
+    let m=1;
+    delete books[isbn].reviews[j];
+    Object.keys(books[isbn].reviews).forEach(element =>{
+      books[isbn].reviews[m]=books[isbn].reviews[element];
+      m=m+1;
+    })
+    delete books[isbn].reviews[l];
+    res.status(200).send(`The review of ${req.session.authorization.username} for book with ISBN ${isbn} has been deleted`);
+  }
+  if(j==0){
+    res.status(208).send(`The username ${req.session.authorization.username} does not have any review for book with ISBN ${isbn}`);
+  }
 });
 
 module.exports.authenticated = regd_users;
